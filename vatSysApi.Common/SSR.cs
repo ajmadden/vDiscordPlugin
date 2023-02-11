da19b327fcac52a761db3ba8337f8e9f2b11931a
ajdunsmuir <46506175+ajdunsmuir@users.noreply.github.com>
2023-02-11 15:56:16 +1000

    Fixed issues with SSR codes.

--- vatSysApi.Common/SSR.cs
+++ vatSysApi.Common/SSR.cs
@@ -9,40 +9,16 @@ namespace vatSysApi.Common
 
         public SSR(FDP2.FDR fdr) : this()
         {
-            assigned = fdr.AssignedSSRCode;
+            Assigned = ToString(fdr.AssignedSSRCode);
         }
 
-        private int assigned { get; set; } = 0;
-        public string Assigned
-        {
-            get
-            {
-                if (assigned == 0) return null;
-                return ToString(assigned);
-            }
-            set
-            {
-                assigned = ToInt(value);
-            }
-        }
-        private int actual { get; set; } = 0;
-        public string Actual
-        {
-            get
-            {
-                return ToString(actual);
-            }
-            set
-            {
-                actual = ToInt(value);
-            }
-        }
+        public string Assigned { get; set; }
+        public string Actual { get; set; }
         public bool IsCorrect
         {
             get
             {
-                if (assigned == 0 && actual != 0) return true;
-                if (assigned == actual) return true;
+                if (Assigned == Actual) return true;
                 return false;
             }
         }
@@ -50,36 +26,33 @@ namespace vatSysApi.Common
         {
             get
             {
-                if (assigned == 0) return false;
-                return true;
+                return !string.IsNullOrWhiteSpace(Assigned);
             }
         }
         public bool IsModeC { get; set; }
 
         public void FDRUpdate(FDP2.FDR updated)
         {
-            assigned = updated.AssignedSSRCode;
+            Assigned = ToString(updated.AssignedSSRCode);
         }
 
         public void RadarUpdate(RDP.RadarTrack updated)
         {
-            actual = updated.ActualAircraft.TransponderCode;
+            Actual = ToString(updated.ActualAircraft.TransponderCode);
             IsModeC = updated.ActualAircraft.TransponderModeC;
         }
 
         private int ToInt(string input)
         {
-            try
-            {
-                return Convert.ToInt32(input, 8);
-            }
-            catch { return 0; }
+            return Convert.ToInt32(input, 8);
         }
 
         private string ToString(int input)
         {
             if (input == 0) return null;
-            return Convert.ToString(input, 8);
+            var converted = Convert.ToString(input, 8);
+            if (converted == "37777777777") return null;
+            return converted;
         }
     }
 }
